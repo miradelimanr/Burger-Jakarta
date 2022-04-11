4ae2305bc0719acc09230099296574926dc4a1ff
miradelimanr <99705748+miradelimanr@users.noreply.github.com>
2022-04-11 17:03:34 +0700

    Add files via upload

---
+++ Central Jakarta/Price.sql
@@ -0,0 +1,7 @@
+SELECT partlor_name, district, unique_menu, price FROM central_jakarta 
+WHERE price >= 50000 ORDER BY price DESC;
+SELECT partlor_name, district, unique_menu, price FROM central_jakarta 
+WHERE price <= 50000 ORDER BY price;
+SELECT partlor_name, district, unique_menu, price FROM central_jakarta 
+WHERE price >= 100000 ORDER BY price DESC;
+
