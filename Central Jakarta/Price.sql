c970c81c5d752720e1957576d52bb2fa610db568
miradelimanr <99705748+miradelimanr@users.noreply.github.com>
2022-04-11 17:04:02 +0700

    Delete Price.sql

--- Central Jakarta/Price.sql
+++
@@ -1,7 +0,0 @@
-SELECT partlor_name, district, unique_menu, price FROM central_jakarta 
-WHERE price >= 50000 ORDER BY price DESC;
-SELECT partlor_name, district, unique_menu, price FROM central_jakarta 
-WHERE price <= 50000 ORDER BY price;
-SELECT partlor_name, district, unique_menu, price FROM central_jakarta 
-WHERE price >= 100000 ORDER BY price DESC;
-
