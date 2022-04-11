9b80fc64084995b6adfe49aea20058deb8216c4b
miradelimanr <99705748+miradelimanr@users.noreply.github.com>
2022-04-11 12:51:17 +0700

    Add files via upload

---
+++ Central Jakarta District Filtering.sql
@@ -0,0 +1,47 @@
+SELECT * FROM burger.central_jakarta;
+SELECT partlor_name, district, unique_menu, price FROM central_jakarta;
+
+#district filter
+SELECT DISTINCT district FROM central_jakarta ORDER BY district;
+SELECT partlor_name, district, unique_menu, price, dining, takeaway, delivery, outdoor_seat, 
+smoking_area, alcohol_served, wifi, reservation, halal_food FROM central_jakarta 
+WHERE district = "Bendungan Hilir";
+SELECT partlor_name, district, unique_menu, price, dining, takeaway, delivery, outdoor_seat, 
+smoking_area, alcohol_served, wifi, reservation, halal_food FROM central_jakarta 
+WHERE district = "Cempaka Putih";
+SELECT partlor_name, district, unique_menu, price, dining, takeaway, delivery, outdoor_seat, 
+smoking_area, alcohol_served, wifi, reservation, halal_food FROM central_jakarta 
+WHERE district = "Hayam Wuruk";
+SELECT partlor_name, district, unique_menu, price, dining, takeaway, delivery, outdoor_seat, 
+smoking_area, alcohol_served, wifi, reservation, halal_food FROM central_jakarta 
+WHERE district = "Karet";
+SELECT partlor_name, district, unique_menu, price, dining, takeaway, delivery, outdoor_seat, 
+smoking_area, alcohol_served, wifi, reservation, halal_food FROM central_jakarta 
+WHERE district = "Menteng";
+SELECT partlor_name, district, unique_menu, price, dining, takeaway, delivery, outdoor_seat, 
+smoking_area, alcohol_served, wifi, reservation, halal_food FROM central_jakarta 
+WHERE district = "Pecenongan";
+SELECT partlor_name, district, unique_menu, price, dining, takeaway, delivery, outdoor_seat, 
+smoking_area, alcohol_served, wifi, reservation, halal_food FROM central_jakarta 
+WHERE district = "SCBD";
+SELECT partlor_name, district, unique_menu, price, dining, takeaway, delivery, outdoor_seat, 
+smoking_area, alcohol_served, wifi, reservation, halal_food FROM central_jakarta 
+WHERE district = "Senen";
+SELECT partlor_name, district, unique_menu, price, dining, takeaway, delivery, outdoor_seat, 
+smoking_area, alcohol_served, wifi, reservation, halal_food FROM central_jakarta 
+WHERE district = "Sudirman";
+SELECT partlor_name, district, unique_menu, price, dining, takeaway, delivery, outdoor_seat, 
+smoking_area, alcohol_served, wifi, reservation, halal_food FROM central_jakarta 
+WHERE district = "Tanah Abang";
+SELECT partlor_name, district, unique_menu, price, dining, takeaway, delivery, outdoor_seat, 
+smoking_area, alcohol_served, wifi, reservation, halal_food FROM central_jakarta 
+WHERE district = "Thamrin"; 
+
+#partlor_name
+SELECT partlor_name, district, google_rating, google_count, qraved_rating, qraved_count, 
+zomato_rating, zomato_count, TRUNCATE((google_rating + qraved_rating + zomato_rating)/3, 1) AS total_rating, (google_count + qraved_count + zomato_count) AS total_count,
+CASE 
+	WHEN (google_rating + qraved_rating + zomato_rating)/3 >= 3 THEN "Recommended"
+	ELSE "Reconsidered"
+END AS "Recommendation" 
+FROM central_jakarta;
