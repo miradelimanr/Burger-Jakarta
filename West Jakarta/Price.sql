1da5dc234b88d950166f6be3016d811da2e6f9bf
miradelimanr <99705748+miradelimanr@users.noreply.github.com>
2022-04-12 14:48:05 +0700

    Update Price.sql

--- West Jakarta/Price.sql
+++ West Jakarta/Price.sql
@@ -1,12 +1,68 @@
-SELECT partlor_name, district, unique_menu, price FROM south_jakarta ORDER BY price DESC;
-SELECT partlor_name, district, unique_menu, price FROM south_jakarta ORDER BY price;
+SELECT partlor_name, district, unique_menu, price 
+FROM south_jakarta 
+ORDER BY price DESC;
 
-SELECT partlor_name, district, unique_menu, price, dining, takeaway, delivery, outdoor_seat, 
-smoking_area, alcohol_served, wifi, reservation, halal_food FROM west_jakarta 
-WHERE price >= 50000;
-SELECT partlor_name, district, unique_menu, price, dining, takeaway, delivery, outdoor_seat, 
-smoking_area, alcohol_served, wifi, reservation, halal_food FROM west_jakarta 
-WHERE price <= 50000;
-SELECT partlor_name, district, unique_menu, price, dining, takeaway, delivery, outdoor_seat, 
-smoking_area, alcohol_served, wifi, reservation, halal_food FROM west_jakarta 
+---OUTPUT (10 rows)
+# partlor_name			district	unique_menu			price
+TGI Fridays Central Park	Tanjung Duren	Fridays Cheeseburgers		129000
+Carls Jr. Central Park		Tanjung Duren	Super Star Cheese		79900
+Carls Jr. Lippo Mall Puri	Puri Indah	Western Beefbac Cheeseburger	69901
+Burger King Mall Ciputra	Grogol		Double Whooper			69091
+Burger King Central Park	Tanjung Duren	Chicken Steakhouse BBQ		59091
+Burger King Cengkareng		Cengkareng	Extra King 1			52000
+Burger King Green Sedayu Mall	Cengkareng	Extra King 1			52000
+Burger King Mall Taman Anggrek	Tanjung Duren	Chicken Tenderstrip		42727
+Flip Burger Meruya		Meruya		Cheese Burger			30000
+Flip Burger Kebon Jeruk		Kebon Jeruk	Brisket Cheese Burger		28182
+---
+
+SELECT partlor_name, district, unique_menu, price 
+FROM south_jakarta 
+ORDER BY price;
+
+---OUTPUT (10 rows)
+# partlor_name			district	unique_menu			price
+Flip Burger Kebon Jeruk		Kebon Jeruk	Brisket Cheese Burger		28182
+Flip Burger Meruya		Meruya		Cheese Burger			30000
+Burger King Mall Taman Anggrek	Tanjung Duren	Chicken Tenderstrip		42727
+Burger King Cengkareng		Cengkareng	Extra King 1			52000
+Burger King Green Sedayu Mall	Cengkareng	Extra King 1			52000
+Burger King Central Park	Tanjung Duren	Chicken Steakhouse BBQ		59091
+Burger King Mall Ciputra	Grogol		Double Whooper			69091
+Carls Jr. Lippo Mall Puri	Puri Indah	Western Beefbac Cheeseburger	69901
+Carls Jr. Central Park		Tanjung Duren	Super Star Cheese		79900
+TGI Fridays Central Park	Tanjung Duren	Fridays Cheeseburgers		129000
+---
+
+SELECT partlor_name, district, unique_menu, price
+FROM west_jakarta 
 WHERE price >= 100000;
+
+---OUTPUT (1 row)
+# partlor_name			district	unique_menu		price
+TGI Fridays Central Park	Tanjung Duren	Fridays Cheeseburgers	129000
+
+SELECT partlor_name, district, unique_menu, price 
+FROM west_jakarta 
+WHERE price BETWEEN 50000 AND 100000;
+
+---OUTPUT (6 rows)
+# partlor_name			district	unique_menu			price
+Burger King Cengkareng		Cengkareng	Extra King 1			52000
+Burger King Green Sedayu Mall	Cengkareng	Extra King 1			52000
+Burger King Central Park	Tanjung Duren	Chicken Steakhouse BBQ		59091
+Burger King Mall Ciputra	Grogol		Double Whooper			69091
+Carls Jr. Lippo Mall Puri	Puri Indah	Western Beefbac Cheeseburger	69901
+Carls Jr. Central Park		Tanjung Duren	Super Star Cheese		79900
+---
+
+SELECT partlor_name, district, unique_menu, price
+FROM west_jakarta 
+WHERE price <= 50000;
+
+---OUTPUT (3 rows)
+# partlor_name			district	unique_menu		price
+Burger King Mall Taman Anggrek	Tanjung Duren	Chicken Tenderstrip	42727
+Flip Burger Meruya		Meruya		Cheese Burger		30000
+Flip Burger Kebon Jeruk		Kebon Jeruk	Brisket Cheese Burger	28182
+---
