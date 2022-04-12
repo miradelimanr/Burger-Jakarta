01438c3b9d0dc52ca24c272613be0392dee8bc07
miradelimanr <99705748+miradelimanr@users.noreply.github.com>
2022-04-12 08:47:49 +0700

    Add files via upload

---
+++ East Jakarta/Price.sql
@@ -0,0 +1,11 @@
+#prices
+SELECT partlor_name, district, unique_menu, price, dining, takeaway, delivery, outdoor_seat, 
+smoking_area, alcohol_served, wifi, reservation, halal_food FROM east_jakarta 
+WHERE price >= 50000;
+SELECT partlor_name, district, unique_menu, price, dining, takeaway, delivery, outdoor_seat, 
+smoking_area, alcohol_served, wifi, reservation, halal_food FROM east_jakarta 
+WHERE price <= 50000;
+SELECT partlor_name, district, unique_menu, price, dining, takeaway, delivery, outdoor_seat, 
+smoking_area, alcohol_served, wifi, reservation, halal_food FROM east_jakarta 
+WHERE price >= 100000;
+
