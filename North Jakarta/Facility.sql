ec990841921fbc9294c69452ba91004f7c70de0e
miradelimanr <99705748+miradelimanr@users.noreply.github.com>
2022-04-12 11:44:50 +0700

    Update Facility.sql

--- North Jakarta/Facility.sql
+++ North Jakarta/Facility.sql
@@ -1,25 +1,179 @@
-#availability
-SELECT partlor_name, district, unique_menu, price, dining FROM north_jakarta 
+SELECT partlor_name, district, unique_menu, price, dining 
+FROM north_jakarta 
 WHERE dining = "yes";
-SELECT partlor_name, district, unique_menu, price, delivery FROM north_jakarta 
+
+---OUTPUT (11 rows)
+# partlor_name	                district	        unique_menu	                price	dining
+Burger King Pantai Indah Kapuk	Pantai Indah Kapuk	Cheese Burger	                25000	yes
+Burger King Mall Kelapa Gading	Kelapa Gading	        Cheese Burger	                25000	yes
+Burger King Mall Artha Gading	Kelapa Gading	        Cheese Burger	                25000	yes
+Burger King Sunter Mall	        Sunter	                Chicken Steakhouse BBQ	        59091	yes
+Burger +	                Sunter	                Special Bacon & Egg Burger	58000	yes
+Lemoe Burger	                Kelapa Gading	        Beef Cabe Ijo	                17000	yes
+Flip Burger Kelapa Gading	Kelapa Gading	        Brisket Cheese Burger	        28182	yes
+Burger Bener Kelapa Gading	Kelapa Gading	        Bener 1	                        13000	yes
+Five Monkeys 	                Sunter	                BBQ Bacon Cheeseburger	        48182	yes
+Six Ounces Coffee	        Kelapa Gading	        Aussie Burger	                75000	yes
+Pig Hunter	                Sunter	                American Pork Burger	        55000	yes
+---
+
+SELECT partlor_name, district, unique_menu, price, dining 
+FROM north_jakarta
+WHERE takeaway = "yes";
+
+---OUTPUT (11 rows)
+# partlor_name	                district	        unique_menu	                price	takeaway
+Burger King Pantai Indah Kapuk	Pantai Indah Kapuk	Cheese Burger	                25000	yes
+Burger King Mall Kelapa Gading	Kelapa Gading	        Cheese Burger	                25000	yes
+Burger King Mall Artha Gading	Kelapa Gading	        Cheese Burger	                25000	yes
+Burger King Sunter Mall	        Sunter	                Chicken Steakhouse BBQ	        59091	yes
+Burger +	                Sunter	                Special Bacon & Egg Burger	58000	yes
+Lemoe Burger	                Kelapa Gading	        Beef Cabe Ijo	                17000	yes
+Flip Burger Kelapa Gading	Kelapa Gading	        Brisket Cheese Burger	        28182	yes
+Burger Bener Kelapa Gading	Kelapa Gading	        Bener 1	                        13000	yes
+Five Monkeys 	                Sunter	                BBQ Bacon Cheeseburger	        48182	yes
+Six Ounces Coffee	        Kelapa Gading	        Aussie Burger	                75000	yes
+Pig Hunter	                Sunter	                American Pork Burger	        55000	yes
+---
+
+SELECT partlor_name, district, unique_menu, price, delivery 
+FROM north_jakarta 
 WHERE delivery = "yes";
-SELECT partlor_name, district, unique_menu, price, outdoor_seat FROM north_jakarta 
+
+---OUTPUT (7 rows)
+# partlor_name	                district	        unique_menu	        price	delivery
+Burger King Pantai Indah Kapuk	Pantai Indah Kapuk	Cheese Burger	        25000	yes
+Burger King Mall Kelapa Gading	Kelapa Gading	        Cheese Burger	        25000	yes
+Burger King Mall Artha Gading	Kelapa Gading	        Cheese Burger	        25000	yes
+Burger King Sunter Mall	Sunter	Chicken                 Steakhouse BBQ	        59091	yes
+Lemoe Burger	                Kelapa Gading	        Beef Cabe Ijo	        17000	yes
+Burger Bener Kelapa Gading	Kelapa Gading	        Bener 1	                13000	yes
+Five Monkeys 	                Sunter	                BBQ Bacon Cheeseburger	48182	yes
+---
+
+SELECT partlor_name, district, unique_menu, price, outdoor_seat 
+FROM north_jakarta 
 WHERE outdoor_seat = "yes";
-SELECT partlor_name, district, unique_menu, price, smoking_area FROM north_jakarta 
+
+---OUTPUT (3 rows)
+# partlor_name	                district	        unique_menu	price	outdoor_seat
+Burger King Pantai Indah Kapuk	Pantai Indah Kapuk	Cheese Burger	25000	yes
+Lemoe Burger	                Kelapa Gading	        Beef Cabe Ijo	17000	yes
+Six Ounces Coffee	        Kelapa Gading	        Aussie Burger	75000	yes
+---
+
+SELECT partlor_name, district, unique_menu, price, smoking_area 
+FROM north_jakarta 
 WHERE smoking_area = "yes";
-SELECT partlor_name, district, unique_menu, price, alcohol_served FROM north_jakarta 
+
+---OUTPUT (6 rows)
+# partlor_name	                district	        unique_menu	        price	smoking_area
+Burger King Pantai Indah Kapuk	Pantai Indah Kapuk	Cheese Burger	        25000	yes
+Flip Burger Kelapa Gading	Kelapa Gading	        Brisket Cheese Burger	28182	yes
+Burger Bener Kelapa Gading	Kelapa Gading	        Bener 1	                13000	yes
+Five Monkeys 	                Sunter	                BBQ Bacon Cheeseburger	48182	yes
+Six Ounces Coffee	        Kelapa Gading	        Aussie Burger	        75000	yes
+Pig Hunter	                Sunter	                American Pork Burger	55000	yes
+---
+
+SELECT partlor_name, district, unique_menu, price, alcohol_served 
+FROM north_jakarta 
 WHERE alcohol_served = "yes";
-SELECT partlor_name, district, unique_menu, price, alcohol_served FROM north_jakarta 
+
+---OUTPUT (2 rows)
+# partlor_name	district	unique_menu	        price	alcohol_served
+Five Monkeys 	Sunter	        BBQ Bacon Cheeseburger	48182	yes
+Pig Hunter	Sunter  	American Pork Burger	55000	yes
+---
+
+SELECT partlor_name, district, unique_menu, price, alcohol_served 
+FROM north_jakarta 
 WHERE alcohol_served = "no";
-SELECT partlor_name, district, unique_menu, price, wifi FROM north_jakarta 
+
+---OUTPUT (9 rows)
+# partlor_name	                district	        unique_menu	                price	alcohol_served
+Burger King Pantai Indah Kapuk	Pantai Indah Kapuk	Cheese Burger	                25000	no
+Burger King Mall Kelapa Gading	Kelapa Gading	        Cheese Burger	                25000	no
+Burger King Mall Artha Gading	Kelapa Gading	        Cheese Burger	                25000	no
+Burger King Sunter Mall	        Sunter	                Chicken Steakhouse BBQ	        59091	no
+Burger +	                Sunter	                Special Bacon & Egg Burger	58000	no
+Lemoe Burger	                Kelapa Gading	        Beef Cabe Ijo	                17000	no
+Flip Burger Kelapa Gading	Kelapa Gading	        Brisket Cheese Burger	        28182	no
+Burger Bener Kelapa Gading	Kelapa Gading	        Bener 1	                        13000	no
+Six Ounces Coffee	        Kelapa Gading	        Aussie Burger	                75000	no
+---
+
+SELECT partlor_name, district, unique_menu, price, wifi 
+FROM north_jakarta 
 WHERE wifi = "yes";
-SELECT partlor_name, district, unique_menu, price, reservation FROM north_jakarta 
+
+---OUTPUT (7 rows)
+# partlor_name	                district	        unique_menu	        price	wifi
+Burger King Pantai Indah Kapuk	Pantai Indah Kapuk	Cheese Burger	        25000	yes
+Burger King Mall Kelapa Gading	Kelapa Gading	        Cheese Burger	        25000	yes
+Burger King Sunter Mall	        Sunter	                Chicken Steakhouse BBQ	59091	yes
+Flip Burger Kelapa Gading	Kelapa Gading	        Brisket Cheese Burger	28182	yes
+Five Monkeys 	                Sunter	                BBQ Bacon Cheeseburger	48182	yes
+Six Ounces Coffee	        Kelapa Gading	        Aussie Burger	        75000	yes
+Pig Hunter	                Sunter	                American Pork Burger	55000	yes
+---
+
+SELECT partlor_name, district, unique_menu, price, reservation 
+FROM north_jakarta 
 WHERE reservation  = "yes";
-SELECT partlor_name, district, unique_menu, price, reservation FROM north_jakarta 
+
+---OUTPUT (2 rows)
+# partlor_name	        district	unique_menu	        price	reservation
+Pig Hunter	        Sunter	        American Pork Burger	55000	yes
+Six Ounces Coffee	Kelapa Gading	Aussie Burger	        75000	yes
+---
+
+SELECT partlor_name, district, unique_menu, price, reservation 
+FROM north_jakarta 
 WHERE reservation = "no";
-SELECT partlor_name, district, unique_menu, price, drive_thru FROM north_jakarta 
+
+---OUTPUT (9 rows)
+# partlor_name	                district	        unique_menu	                price	reservation
+Burger King Pantai Indah Kapuk	Pantai Indah Kapuk	Cheese Burger	                25000	no
+Burger King Mall Kelapa Gading	Kelapa Gading	        Cheese Burger	                25000	no
+Burger King Mall Artha Gading	Kelapa Gading	        Cheese Burger	                25000	no
+Burger King Sunter Mall	        Sunter	                Chicken Steakhouse BBQ	        59091	no
+Burger +	                Sunter	                Special Bacon & Egg Burger	58000	no
+Lemoe Burger	                Kelapa Gading	        Beef Cabe Ijo	                17000	no
+Flip Burger Kelapa Gading	Kelapa Gading	        Brisket Cheese Burger	        28182	no
+Burger Bener Kelapa Gading	Kelapa Gading	        Bener 1	                        13000	no
+Five Monkeys 	                Sunter	                BBQ Bacon Cheeseburger	        48182	no
+---
+
+SELECT partlor_name, district, unique_menu, price, drive_thru 
+FROM north_jakarta 
 WHERE drive_thru = "yes";
-SELECT partlor_name, district, unique_menu, price, halal_food FROM north_jakarta 
+
+---OUTPUT (0 rows)
+---
+
+SELECT partlor_name, district, unique_menu, price, halal_food 
+FROM north_jakarta 
 WHERE halal_food = "yes";
-SELECT partlor_name, district, unique_menu, price, halal_food FROM north_jakarta 
+
+# partlor_name	                district	        unique_menu	                price	halal_food
+Burger King Pantai Indah Kapuk	Pantai Indah Kapuk	Cheese Burger	                25000	yes
+Burger King Mall Kelapa Gading	Kelapa Gading	        Cheese Burger	                25000	yes
+Burger King Mall Artha Gading	Kelapa Gading	        Cheese Burger	                25000	yes
+Burger King Sunter Mall	        Sunter  	        Chicken Steakhouse BBQ	        59091	yes
+Burger +	                Sunter	                Special Bacon & Egg Burger	58000	yes
+Lemoe Burger	                Kelapa Gading	        Beef Cabe Ijo	                17000	yes
+Flip Burger Kelapa Gading	Kelapa Gading	        Brisket Cheese Burger	        28182	yes
+Burger Bener Kelapa Gading	Kelapa Gading	        Bener 1	                        13000	yes
+Five Monkeys 	                Sunter	                BBQ Bacon Cheeseburger	        48182	yes
+
+
+SELECT partlor_name, district, unique_menu, price, halal_food 
+FROM north_jakarta 
 WHERE halal_food = "no";
+
+---OUTPUT (2 rows)
+# partlor_name	        district	unique_menu	        price	halal_food
+Pig Hunter	        Sunter	        American Pork Burger	55000	no
+Six Ounces Coffee	Kelapa Gading	Aussie Burger	        75000	no
+---
