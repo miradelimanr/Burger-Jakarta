b1030faa9b7f3106e2c48fa389c75d43e359546a
miradelimanr <99705748+miradelimanr@users.noreply.github.com>
2022-04-12 11:12:04 +0700

    Update Price.sql

--- East Jakarta/Price.sql
+++ East Jakarta/Price.sql
@@ -1,4 +1,39 @@
-#prices
+#most expensives
+SELECT partlor_name, district, unique_menu, price 
+FROM east_jakarta 
+ORDER BY price DESC;
+
+---OUTPUT (9 rows)
+# partlor_name				district	unique_menu				price
+Carls Jr. Klender			Klender		Double Western Beefbac			82727
+Burger King Klender			Klender		Chicken Tendercrisp			42727
+Resto Burger Takkatak			Kramat Jati	Extra Double Beef & Cheese Burger	30000
+Burger King Cibubur Junction		Cibubur		Cheese Burger				25000
+The Lime Roaster & Burger		Pulo Gadung	Chicken Burger				25000
+Lemoe Burger Pulo Gadung		Pulo Gadung	Chicken Special Cheese			20000
+Lemoe Burger Matraman			Matraman	Chicken Cabe Ijo			14000
+Papitos Burger				Kramat Jati	Mozarella Burger			14000
+Dons Burger Pusat Grosir Cililitan	Cililitan	Spesial Burger				13000
+---
+
+#cheapest as possible
+SELECT partlor_name, district, unique_menu, price 
+FROM east_jakarta 
+ORDER BY price;
+
+---OUTPUT (9 rows)
+# partlor_name				district		unique_menu				price
+Dons Burger Pusat Grosir Cililitan	Cililitan		Spesial Burger				13000
+Lemoe Burger Matraman			Matraman		Chicken Cabe Ijo			14000
+Papitos Burger				Kramat Jati		Mozarella Burger			14000
+Lemoe Burger Pulo Gadung		Pulo Gadung		Chicken Special Cheese			20000
+Burger King Cibubur Junction		Cibubur			Cheese Burger				25000
+The Lime Roaster & Burger		Pulo Gadung		Chicken Burger				25000
+Resto Burger Takkatak			Kramat Jati		Extra Double Beef & Cheese Burger	30000
+Burger King 				Klender	Klender		Chicken Tendercrisp			42727
+Carls Jr. 				Klender	Klender		Double Western Beefbac			82727
+---
+
 SELECT partlor_name, district, unique_menu, price, dining, takeaway, delivery, outdoor_seat, smoking_area, alcohol_served, wifi, reservation, halal_food 
 FROM east_jakarta 
 WHERE price >= 50000;
