3d522d0b040bdb1a7e4f79ec618b1c85eda3003f
miradelimanr <99705748+miradelimanr@users.noreply.github.com>
2022-04-12 15:09:11 +0700

    Update Facility.sql

--- South Jakarta/Facility.sql
+++ South Jakarta/Facility.sql
@@ -168,34 +168,6 @@ WHERE drive_thru = "yes";
 
 SELECT partlor_name, district, unique_menu, price, halal_food 
 FROM south_jakarta 
-WHERE halal_food = "yes";
-
----OUTPUT (20 rows)
-# partlor_name			district	unique_menu			price	halal_food
-Burger King Blok M		Blok M		Double Cheese Burger		41818	yes
-Burger King Tebet		Tebet		Double Cheese Burger		41818	yes
-Burger King Lotte Avenue	Karet		Double Cheese Burger		41818	yes
-Burger King Bellagio		Kuningan	Double Cheese Burger		41818	yes
-Burger King Pondok Indah Plaza	Pondok Indah	Double Cheese Burger		41818	yes
-Burger King Kemang		Kemang		Double Cheese Burger		41818	yes
-Burger King Setiabudi		Setiabudi	Double Cheese Burger		41818	yes
-Burger Bakar Qebul		Pasar Minggu	Beef Burger			13000	yes
-Flip Burger Senopati		Senopati	Double Cheese Burger		45000	yes
-Blenger Burger Blok M		Blok M		Cheese Burger X2		32000	yes
-Blenger Burger Fatmawati	Fatmawati	Cheese Burger			20000	yes
-Goods Burger			Pondok Indah	Goods Coffee Rub		65000	yes
-Flip Burger Kota Kasablanka	Tebet		Brisket Cheese Burger		28182	yes
-Flip Burger Pondok Indah Plaza	Pondok Indah	Bitsy Double Cheese		45455	yes
-Lawless Burgerbar		Kemang		Philly Anselmo			75000	yes
-Byurger				Fatmawati	Sexyback Rib			65000	yes
-TGI Fridays Lippo Mall Kemang	Kemang		Fridays Cheeseburger		129000	yes
-Carls Junior			Kemang		Western Beefbac			59091	yes
-MOS Cafe			SCBD		Charcoal Tobikiri Burger	63000	yes
-Stacksite Express		Senayan		Quicksand			30000	yes
----
-
-SELECT partlor_name, district, unique_menu, price, halal_food 
-FROM south_jakarta 
 WHERE halal_food = "no";
 
 ---OUTPUT (1 row)
