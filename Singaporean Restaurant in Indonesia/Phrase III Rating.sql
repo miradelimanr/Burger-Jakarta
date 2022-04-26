da58164fa385ebfa8e25f2157909a41a3e055504
Mira Delima Noor <99705748+miradelimanr@users.noreply.github.com>
2022-04-26 09:40:47 +0700

    Delete Singaporean Restaurant in Indonesia directory

--- Singaporean Restaurant in Indonesia/Phrase III Rating.sql
+++
@@ -1,7 +0,0 @@
-SELECT restaurant_name, city, google_rating, platform_rating, TRUNCATE((google_rating + platform_rating)/2, 1) as total_rating,
-CASE
-	WHEN (google_rating + platform_rating)/2 >= 3.5 THEN "Recommended"
-    ELSE "Reconsider"
-END AS "Recommendation"
-FROM singapore.singapore_id WHERE region LIKE '%Sumatra' OR region LIKE '%Kalimantan' OR region LIKE '%Sulawesi' OR region LIKE '%Papua'
-ORDER BY total_rating DESC;
